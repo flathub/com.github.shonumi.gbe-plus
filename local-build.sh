975992b7827fcb1a4ab9d9736f8b43b7660d6759
Bastien Nocera <hadess@hadess.net>
2021-04-01 09:44:27 +0200

    Pass --default-branch=test to the test build script

--- local-build.sh
+++ local-build.sh
@@ -4,6 +4,6 @@ rm -f com.github.shonumi.gbe-plus.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build com.github.shonumi.gbe-plus.json --repo=_repo
-flatpak build-bundle _repo com.github.shonumi.gbe-plus.flatpak com.github.shonumi.gbe-plus master
+flatpak-builder --ccache --force-clean --default-branch=test _build com.github.shonumi.gbe-plus.json --repo=_repo
+flatpak build-bundle _repo com.github.shonumi.gbe-plus.flatpak com.github.shonumi.gbe-plus test
 
